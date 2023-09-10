a75193ae676befac4a4021e4ab069ec9743bf695
ccc007 <1492949083@qq.com>
2023-09-10 21:30:40 +0800

    为主题功能铺路

---
+++ js/base.js
@@ -0,0 +1,8 @@
+const root = document.documentElement;
+// 改变 颜色 变量
+// 默认配色
+// root.style.setProperty('--bg-color', 'rgb(34, 34, 49)');
+// root.style.setProperty('--bg-Dcolor', 'rgb(17, 17, 36)');
+// root.style.setProperty('--linear-gradient-bg-color', 'linear-gradient(to bottom, var(--bg-color) 30%,var(--bg-Dcolor))');
+// root.style.setProperty('--text-color', 'rgb(253, 237, 202)');
+// root.style.setProperty('--text-color-accent', 'rgb(255, 201, 120)');
