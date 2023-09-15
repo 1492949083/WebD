15aaf0d4ca9581dbdf242a70db3451b4f5942f2d
ccc007 <1492949083@qq.com>
2023-09-16 04:42:13 +0800

    完善

--- js/navBar.js
+++ js/navBar.js
@@ -5,15 +5,9 @@ $("#menu").menu();
 // 设置菜单选项
 $("#menu").menu({
     items: "> :not(:first-child)",
-    position: { my: "left top", at: "left bottom" }  
+    position: { my: "left top", at: "left ", of: $("") }
 });
-
-// 打开二级菜单
-$("#menu").on("click", "li", function () {
-    $(this).children("ul").show();
-});
-
-// 关闭二级菜单 
-$("#menu").on("click", "a", function () {
-    $(this).parent().parent().children("ul").hide();
+// 设置除了二级菜单所有菜单选项向右边展开
+$("#menu > li > .p_ul1").menu({
+    position: { my: "left top", at: "right top+9%", of: $(".p_ul1") }
 });
